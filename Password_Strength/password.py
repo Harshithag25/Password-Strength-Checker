04d416d40e6fea28cec3c8c908cb5cb3c3134a11
Harshitha Gontla <harshitag335@gmail.com>
2026-05-22 16:49:32 +0530

    Refactor password strength checker for clarity

--- Password_Strength/password.py
+++ Password_Strength/password.py
@@ -1,10 +1,12 @@
 import string
 class PasswordStrengthChecker:
+    
     def strength(self,s):
         lower=False
         upper=False
         digit=False
         special=False
+        
         for i in s:
             if i.isupper()==True:
                 upper=True
@@ -14,6 +16,7 @@ class PasswordStrengthChecker:
                 digit=True
             elif i in string.punctuation:
                 special=True
+                
         if len(s)<8:
             return "Invalid password"
         if (lower==True and upper==True and digit==True and special==True):
@@ -23,9 +26,11 @@ class PasswordStrengthChecker:
         else:
             print("Your password is Weak")
         print("Thank You")
+        
 print("------Password Strength Checker------")
+
 print("The constraints are: 1.Length of password is 8 2.Contain upper characters 3.Contain lower characters 4.Contains digits 5.Contains special symbols")
 p=input("Enter your Password: ")
+
 check=PasswordStrengthChecker()
 result=check.strength(p)
-#print(result)
